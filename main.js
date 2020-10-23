920acf9962435d731591acf7112e3d6a5e86590d
svati08-Amazon <patelsv1822@gmail.com>
2020-10-23 17:46:34 -0400

    my second Dice Roller

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let rollDiceInput = document.querySelector("#roll-number");
 let rollButton = document.querySelector("#roll-dice");
 let ShowAllRolls = document.querySelector("#show-all-rolls");
-let total = document.querySelector("#total");
+let total = document.querySelector("#sum");
 let rollresults = document.querySelector("#roll-results");
 let resetButton = document.querySelector("#reset-button");
 let allRolls = [];
@@ -9,11 +9,12 @@ let allRolls = [];
 function giveMeRandomNumber() {
   return Math.floor(Math.random() * 6) + 1;
 }
+
 rollButton.addEventListener("click", function () {
   console.log(rollDiceInput.value);
   let maxRolls = Number(rollDiceInput.value);
   let count = 0;
-  //   rollDiceInput.value = "";
+  // rollDiceInput.value = "";
   while (count < maxRolls) {
     allRolls.push(giveMeRandomNumber());
     count++;
@@ -21,17 +22,14 @@ rollButton.addEventListener("click", function () {
   console.log(allRolls.reduce((a, b) => a + b, 0));
   console.log(allRolls);
 });
-// total.innerHTML = allRolls;
-// console.log(total);
 
 ShowAllRolls.addEventListener("click", function () {
-  //console.log(ShowAllRolls);
+  // console.log(ShowAllRolls);
   rollresults.innerHTML = "";
   let count = 0;
   while (count < allRolls.length) {
-    //console.log(allRolls[count]);
-
-    rollresults.innerHTML += "<1i>" + allRolls[count] + "</1i>";
+    // console.log(allRolls[count]);
+    rollresults.innerHTML += "<li>" + allRolls[count] + "</li>";
     count++;
   }
 });
@@ -39,7 +37,6 @@ ShowAllRolls.addEventListener("click", function () {
 resetButton.addEventListener("click", function () {
   console.log("");
   let resetButton = 0;
-
   while (allRolls < resetButton.value) {
     allRolls.innerHTML = resetButton;
   }
